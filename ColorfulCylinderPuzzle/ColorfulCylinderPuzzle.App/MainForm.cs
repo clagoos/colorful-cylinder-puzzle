7bd69e5d738efad41464cbc2bf6d7f3b15185b1f
Lech Osinski <lech.osinski@bms.ad>
2016-08-02 12:22:12 +0200

    drawing rectangles!

--- ColorfulCylinderPuzzle/ColorfulCylinderPuzzle.App/MainForm.cs
+++ ColorfulCylinderPuzzle/ColorfulCylinderPuzzle.App/MainForm.cs
@@ -1,32 +1,64 @@
-﻿using System.Drawing;
+﻿using ColorfulCylinderPuzzle.Logic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
-using ColorfulCylinderPuzzle.Logic;
 
 namespace ColorfulCylinderPuzzle.App
 {
     public partial class MainForm : Form
     {
-        private Puzzle puzzle;
+        private readonly Puzzle puzzle;
+        private readonly Dictionary<Colors, SolidBrush> colorBrushes;
+
         private Rectangle[,] puzzlePieces;
 
         public MainForm()
         {
             InitializeComponent();
             puzzle = StandardPuzzleInitializer.GetUpperSolvedPuzzle();
+            colorBrushes = InitializeColorBrushes();
+        }
+
+        private void InitializeRectangles(int panelWidth, int panelHeight)
+        {
             puzzlePieces = new Rectangle[puzzle.Rows, puzzle.Columns];
+            var singleItemWidth = panelWidth / puzzle.Columns;
+            var singleItemHeight = panelHeight / puzzle.Rows;
+            var size = new Size(singleItemWidth, singleItemHeight);
+            for (int row = 0; row < puzzle.Rows; row++)
+            {
+                for (int column = 0; column < puzzle.Columns; column++)
+                {
+                    var position = new Point(column * singleItemWidth, row * singleItemHeight);
+                    puzzlePieces[row, column] = new Rectangle(position, size);
+                    puzzlePieces[row, column].Inflate(-2, -2);
+                }
+            }
+        }
+
+        private static Dictionary<Colors, SolidBrush> InitializeColorBrushes()
+        {
+            return Enum.GetNames(typeof(Colors))
+                .ToDictionary(
+                    colorName => (Colors)Enum.Parse(typeof(Colors), colorName),
+                    colorName => new SolidBrush(Color.FromName(colorName)));
         }
 
         private void panelPuzzle_Paint(object sender, PaintEventArgs e)
         {
             var panel = sender as Panel;
             if (panel == null) return;
-            var singleItemWidth = panel.Width / puzzle.Columns;
-            var singleItemHeight = panel.Height / puzzle.Rows;
+            if (puzzlePieces == null)
+                InitializeRectangles(panel.Width, panel.Height);
             for (int row = 0; row < puzzle.Rows; row++)
             {
                 for (int column = 0; column < puzzle.Columns; column++)
                 {
-                    //l: todo: narysuj kwadrat! :)
+                    var brush = colorBrushes[(Colors)puzzle.Map[row, column]];
+                    var rectangle = puzzlePieces[row, column];
+                    e.Graphics.FillRectangle(brush, rectangle);
                 }
             }
         }
